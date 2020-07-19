e488acadd37cabbba8714a8960841e1140cf4ba0
Claudia Lay <claudialay@Claudias-MBP.lan>
2020-07-19 11:21:43 -0500

    installed inquirer

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const fs = require('fs');
-const profileDataArgs = process.argv.slice(2, process.argv.lenth);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+inquirer    
+    .prompt([
+            {
+            type: 'input', 
+            name: 'name', 
+            message: 'What is your name?'
+            }
+        ])
+    .then(answers => console.log(answers));
+
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const profileDataArgs = process.argv.slice(2, process.argv.lenth);
+// const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => 
+// {
+//     if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-module.exports = generatePage;
+// module.exports = generatePage;
